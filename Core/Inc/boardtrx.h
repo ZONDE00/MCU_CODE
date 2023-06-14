74ae2da8b3ea261d29e5630edaf763915daa2a7e
krekers <kristers818@gmail.com>
2023-06-14 20:06:47 +0300

    Unfaking Rodrigo fak up

--- Core/Inc/boardtrx.h
+++ Core/Inc/boardtrx.h
@@ -14,37 +14,40 @@
 #define BOARDTRX_CMD_COUNT        8
 
 typedef enum {
-	BOARDTRX_OK,
-	BOARDTRX_ERROR,
-	BOARDTRX_BUSY,
-	BOARDTRX_TIMEOUT,
-	BOARDTRX_MISS,
-	BOARDTRX_ERROR_CRC,
-	BOARDTRX_ERROR_CMD,
-	BOARDTRX_ERROR_TX,
+    BOARDTRX_OK,
+    BOARDTRX_ERROR,
+    BOARDTRX_BUSY,
+    BOARDTRX_TIMEOUT,
+    BOARDTRX_MISS,
+    BOARDTRX_ERROR_CRC,
+    BOARDTRX_ERROR_CMD,
+    BOARDTRX_ERROR_TX,
 } BOARDTRX_StatusTypeDef;
 
 typedef enum {
-	BOARDTRX_INT, // other error internal error
-	BOARDTRX_TX,
-	BOARDTRX_RX
+    BOARDTRX_INT, // other error internal error
+    BOARDTRX_TX,
+    BOARDTRX_RX
 //    BOARDTRX_ERROR_UART,
 } BOARDTRX_TRX_Status;
 
 typedef enum {
-	BOARDTRX_CMD_DEF,   // default state, nothing is happening
-	BOARDTRX_CMD_NEW,   // new id respond with ack
-	BOARDTRX_CMD_REP,   // same id, already executed
-	BOARDTRX_CMD_NCK,
+    BOARDTRX_CMD_DEF,   // default state, nothing is happening
+    BOARDTRX_CMD_NEW,   // new id respond with ack
+    BOARDTRX_CMD_REP,   // same id, already executed
+    BOARDTRX_CMD_NCK,
 
-	BOARDTRX_CMD_SEN,   // trying to send CMD
-	BOARDTRX_CMD_OK,    // CMD was sent successfully
-	BOARDTRX_CMD_NOK,   // CMD was not sent
-	BOARDTRX_CMD_TIM    // timed out
+    BOARDTRX_CMD_SEN,   // trying to send CMD
+    BOARDTRX_CMD_OK,    // CMD was sent successfully
+    BOARDTRX_CMD_NOK,   // CMD was not sent
+    BOARDTRX_CMD_TIM    // timed out
 } BOARDTRX_CMD_Status;
 
 typedef enum {
-	BOARDTRX_DATA_TX, BOARDTRX_DATA_RX, BOARDTRX_CMD_TX, BOARDTRX_CMD_RX,
+    BOARDTRX_DATA_TX,
+    BOARDTRX_DATA_RX,
+    BOARDTRX_CMD_TX,
+    BOARDTRX_CMD_RX,
 } BOARDTRX_TRX_Target;
 
 /*
@@ -63,63 +66,63 @@ typedef enum {
 
 // data that can be requested from target
 typedef struct {
-	// external
-	uint8_t *data; // data that will be received
-	uint8_t size;  // size of data
-	uint8_t status; // set if is new, clear manually
-	// internal
-	uint8_t retries;
-	uint32_t timeout;
+    // external
+    uint8_t *data; // data that will be received
+    uint8_t size;  // size of data
+    uint8_t status; // set if is new, clear manually
+    // internal
+    uint8_t retries;
+    uint32_t timeout;
 
 } BOARDTRX_TX_Data;
 
 // data that target device can request
 typedef struct {
-	uint8_t *data; // communication with target device
-	uint8_t size;  // size of variable
-	uint8_t wasRequested; // set if was requested, clear manually'
+    uint8_t *data; // communication with target device
+    uint8_t size;  // size of variable
+    uint8_t wasRequested; // set if was requested, clear manually'
 } BOARDTRX_RX_Data;
 
 // cmd that can be received from target device
 typedef struct {
-	// external
-	uint8_t *data; // data that will be received
-	uint8_t size;  // size of data
-	void (*CMD_CB)(); // callback function to call when CMD is received successfully
-	// internal
-	BOARDTRX_CMD_Status status;
-	uint8_t id;
+    // external
+    uint8_t *data; // data that will be received
+    uint8_t size;  // size of data
+    void (*CMD_CB)(); // callback function to call when CMD is received successfully
+    // internal
+    BOARDTRX_CMD_Status status;
+    uint8_t id;
 } BOARDTRX_RX_Cmd;
 
 // data that we can send to target device
 typedef struct {
-	// external
-	uint8_t *data; // data to send to target
-	uint8_t size;  // size of data to send
-	BOARDTRX_CMD_Status status; // BOARDTRX_CMD_DEF when idle, BOARDTRX_CMD_OK when sent successfully, BPARDTRX_CMD_NOK, when was not sent
-								// BOARDTRX_CMD_SEN when sending
-	// internal
-	uint8_t retries;
-	uint32_t timeout;
-	uint8_t id;    // id of last request
-	uint8_t wasSent;
+    // external
+    uint8_t *data; // data to send to target
+    uint8_t size;  // size of data to send
+    BOARDTRX_CMD_Status status; // BOARDTRX_CMD_DEF when idle, BOARDTRX_CMD_OK when sent successfully, BPARDTRX_CMD_NOK, when was not sent
+                                // BOARDTRX_CMD_SEN when sending
+    // internal
+    uint8_t retries;
+    uint32_t timeout;
+    uint8_t id;    // id of last request
+    uint8_t wasSent;
 } BOARDTRX_TX_Cmd;
 
 // not mandatory but can be used to determine TRX quality
 typedef struct {
-	// general status indicators
-	BOARDTRX_StatusTypeDef status; // status of last function
-	uint8_t isNewError; // set if new error happened, clear manually
-	uint8_t errorDataNr; // index of data when last error happened, valid if isNewError was set
-	BOARDTRX_TRX_Status errorTRX; // valid if isNewError was set, marks whether error was on RX or TX
+    // general status indicators
+    BOARDTRX_StatusTypeDef status; // status of last function
+    uint8_t isNewError; // set if new error happened, clear manually
+    uint8_t errorDataNr; // index of data when last error happened, valid if isNewError was set
+    BOARDTRX_TRX_Status errorTRX;    // valid if isNewError was set, marks whether error was on RX or TX
 
-	// error counters
-	uint32_t errorsCRC; // amount of CRC errors that happened
-	uint32_t errorsRX; // amount of unrecognised packets received
-	uint32_t errorsTX; // amount of packets that was not recognised by target device
-	uint32_t errorsTimeout;  // total timeouts
-	uint32_t errorsMissed; // counts how many times new request was received before fulfilling last
-	uint32_t errorsInt; // other internal errors
+    // error counters
+    uint32_t errorsCRC; // amount of CRC errors that happened
+    uint32_t errorsRX; // amount of unrecognised packets received
+    uint32_t errorsTX; // amount of packets that was not recognised by target device
+    uint32_t errorsTimeout;  // total timeouts
+    uint32_t errorsMissed;   // counts how many times new request was received before fulfilling last
+    uint32_t errorsInt; // other internal errors
 } BOARDTRX_Status;
 
 /*
@@ -137,30 +140,30 @@ typedef struct {
  *  Max CMD  count is BOARDTRX_CMD_COUNT,  hard max is 64
  */
 typedef struct {
-	// data that can be received from target device
-	BOARDTRX_TX_Data **dataTx; // pointer to array of pointers
-	uint8_t countDataTx;
+    // data that can be received from target device
+    BOARDTRX_TX_Data **dataTx; // pointer to array of pointers
+    uint8_t countDataTx;
 
-	// data that target device can request
-	BOARDTRX_RX_Data **dataRx; // pointer to array of pointers
-	uint8_t countDataRx;
+    // data that target device can request
+    BOARDTRX_RX_Data **dataRx; // pointer to array of pointers
+    uint8_t countDataRx;
 
-	// CMDs that can be sent to target device
-	BOARDTRX_TX_Cmd **cmdTx; // pointer to array of pointers
-	uint8_t countCmdTx;
+    // CMDs that can be sent to target device
+    BOARDTRX_TX_Cmd **cmdTx; // pointer to array of pointers
+    uint8_t countCmdTx;
 
-	// CMDs that can be received from target device
-	BOARDTRX_RX_Cmd **cmdRx; // pointer to array of pointers
-	uint8_t countCmdRx;
+    // CMDs that can be received from target device
+    BOARDTRX_RX_Cmd **cmdRx; // pointer to array of pointers
+    uint8_t countCmdRx;
 
-	UART_HandleTypeDef *uart; // communication handle with target device
+    UART_HandleTypeDef *uart; // communication handle with target device
 
-	uint8_t rxRetries; // how many times will retry to receive data before giving up, default 3
-	uint32_t rxTimeout; // time in which will rx timeout in ms, default 20'000
+    uint8_t txRetries; // how many times will retry to receive data before giving up, default 3
+    uint32_t txTimeout; // time in which will rx timeout in ms, default 20'000
 
-	void (*TX_COMPLETE_CB)(); // callback when target requested data was sent, not mandatory
-	void (*RX_COMPLETE_CB)(); // callback when requested data was received, not mandatory
-	void (*ERROR_CB)();       // callback on error, not mandatory
+    void (*TX_COMPLETE_CB)(); // callback when target requested data was sent, not mandatory
+    void (*RX_COMPLETE_CB)(); // callback when requested data was received, not mandatory
+    void (*ERROR_CB)();       // callback on error, not mandatory
 } BOARDTRX_Handle;
 
 // ---------------------------------------------------- external functions
--- Core/Src/hq.c
+++ Core/Src/hq.c
@@ -6,6 +6,9 @@
  */
 
 #include "hq.h"
+#include "string.h"
+#include "stdio.h"
+#include "boardtrx.h"
 
 HQ_Handle *hqHandle;
 
@@ -32,24 +35,23 @@ uint32_t timerDelay = 10000;
 BOARDTRX_RX_Data mcuRxBatVolt = { 0 };
 BOARDTRX_RX_Data mcuRx4vCurr = { 0 };
 BOARDTRX_RX_Data mcuRx3v3Curr = { 0 };
+BOARDTRX_RX_Data mcuRx3v3 = { 0 };
 BOARDTRX_RX_Data mcuRxTemp = { 0 };
-BOARDTRX_RX_Data testVarRx = { 0 };
+BOARDTRX_RX_Data mcuRxTempIn = { 0 };
+BOARDTRX_RX_Data mcuRxTempOut = { 0 };
+BOARDTRX_RX_Data mcuRxTempBme = { 0 };
+BOARDTRX_RX_Data mcuRxPressure = { 0 };
+BOARDTRX_RX_Data mcuRxHumid = { 0 };
+BOARDTRX_RX_Data mcuRxAirQ = { 0 };
+BOARDTRX_RX_Data mcuRxGasOhm = { 0 };
+BOARDTRX_RX_Data mcuRxSenState = { 0 };
 
-// TODO replace testVarRx with missing variables
-// variable and cmd pointer arrays, sequence of variables should be like @ref HQ_Commands_TX
-BOARDTRX_RX_Data *rxDataArray[12] = { &mcuRxBatVolt, &mcuRx4vCurr,
-		&mcuRx3v3Curr, &mcuRxTemp, &testVarRx, &testVarRx, &testVarRx,
-		&testVarRx, &testVarRx, &testVarRx, &testVarRx, &testVarRx, };
-
-//HQ_VARS_RX_LONGITUDE,
-//HQ_VARS_RX_LATITUDE,
-//HQ_VARS_RX_ALTITUDE,
-//HQ_VARS_RX_SPEED,
-//HQ_VARS_RX_TIME
-//HQ_VARS_RX_MCU_TEMP,
-//HQ_VARS_RX_CMD_OK,
-//HQ_VARS_RX_CMD_NCK,
-//HQ_VARS_RX_CMD_CRC,
+// variable and cmd pointer arrays, sequence of variables should be like @ref HQ_Commands_RX
+BOARDTRX_RX_Data *rxDataArray[HQ_CMD_RX_END] = {
+        &mcuRxBatVolt, &mcuRx4vCurr, &mcuRx3v3Curr, &mcuRx3v3,
+        &mcuRxTemp, &mcuRxTempIn, &mcuRxTempOut, &mcuRxTempBme,
+        &mcuRxPressure, &mcuRxHumid, &mcuRxAirQ, &mcuRxGasOhm,
+        &mcuRxSenState };
 
 uint8_t gpsLong[12];
 uint8_t gpsLat[12];
@@ -74,9 +76,10 @@ BOARDTRX_TX_Data comTxComCmdCrc = { 0 };
 BOARDTRX_TX_Data comTxComCmdTmo = { 0 };
 
 // data that can be received from COM
-BOARDTRX_TX_Data *txDataArray[10] = { &comTxGpsLong, &comTxGpsLat, &comTxGpsAlt,
-		&comTxGpsSpeed, &comTxGpsTime, &comTxComTemperature, &comTxComCmdOk,
-		&comTxComCmdNck, &comTxComCmdCrc, &comTxComCmdTmo };
+BOARDTRX_TX_Data *txDataArray[HQ_CMD_TX_END] = {
+        &comTxGpsLong, &comTxGpsLat, &comTxGpsAlt, &comTxGpsSpeed,
+        &comTxGpsTime, &comTxComTemperature,
+        &comTxComCmdOk, &comTxComCmdNck, &comTxComCmdCrc, &comTxComCmdTmo};
 
 BOARDTRX_RX_Cmd mcuTxBurn = { 0 };
 BOARDTRX_RX_Cmd comTrxBuzz = { 0 };
@@ -88,16 +91,19 @@ BOARDTRX_RX_Cmd *txCmdArray[2] = { &mcuTxBurn, &comTrxBuzz };
 BOARDTRX_Handle comTrxHandle = { 0 };
 
 // stolen variables, should be put in @ref trxDataArray with their proper structs
-extern uint16_t vref_avg;
-extern uint16_t temp_avg;
 extern float vdda; // Result of VDDA calculation
-extern float vref; // Result of vref calculation
-extern float temp; // Result of temp calculation
-extern float v3_3; // Result of 3.3 ADC calculation
-extern float v4;   // Result of 4 ADC calculation
+extern float temp; // Result of temp calculation ?? TODO is this temperature or temporary, should be temp either way
 extern float i3_3; // Result of 3.3 ADC calculation
 extern float i4;   // Result of 4 ADC calculation
 extern float bat_v; // Result of BAT_V calculation
+extern float temp_in;
+extern float temp_out;
+extern float temp_bme;
+extern float bme_pressure;
+extern float bme_air_q;
+extern float bme_humid;
+extern float bme_air_ohm;
+extern uint32_t senStatus;
 
 // for testing TODO remove once proper variables in @ref trxDataArray are set
 uint32_t testVariable = 2244668800;
@@ -109,95 +115,119 @@ uint32_t testVariable = 2244668800;
  */
 HQ_StatusTypeDef HQ_Init(HQ_Handle *handle) {
 
-	hqHandle = handle;
+    hqHandle = handle;
 
-	// initialize shared variables tx
+    // initialize shared variables tx
 
-	comTxGpsLong.data = (uint8_t*) &gpsLong;
-	comTxGpsLong.size = 12;
+    comTxGpsLong.data = (uint8_t *)&gpsLong;
+    comTxGpsLong.size = 12;
 
-	comTxGpsLat.data = (uint8_t*) &gpsLat;
-	comTxGpsLat.size = 12;
+    comTxGpsLat.data = (uint8_t *)&gpsLat;
+    comTxGpsLat.size = 12;
 
-	comTxGpsAlt.data = (uint8_t*) &gpsAlt;
-	comTxGpsAlt.size = 8;
+    comTxGpsAlt.data = (uint8_t *)&gpsAlt;
+    comTxGpsAlt.size = 8;
 
-	comTxGpsSpeed.data = (uint8_t*) &gpsSpeed;
-	comTxGpsSpeed.size = 7;
+    comTxGpsSpeed.data = (uint8_t *)&gpsSpeed;
+    comTxGpsSpeed.size = 7;
 
-	comTxGpsTime.data = (uint8_t*) &gpsTime;
-	comTxGpsTime.size = 6;
+    comTxGpsTime.data = (uint8_t *)&gpsTime;
+    comTxGpsTime.size = 6;
 
-	comTxComTemperature.data = (uint8_t*) &comTemperature;
-	comTxComTemperature.size = 4;
+    comTxComTemperature.data = (uint8_t *)&comTemperature;
+    comTxComTemperature.size = 4;
 
-	comTxComCmdOk.data = (uint8_t*) &comCmdOk;
-	comTxComCmdOk.size = 4;
+    comTxComCmdOk.data = (uint8_t *)&comCmdOk;
+    comTxComCmdOk.size = 4;
 
-	comTxComCmdNck.data = (uint8_t*) &comCmdNck;
-	comTxComCmdNck.size = 4;
+    comTxComCmdNck.data = (uint8_t *)&comCmdNck;
+    comTxComCmdNck.size = 4;
 
-	comTxComCmdCrc.data = (uint8_t*) &comCmdCrc;
-	comTxComCmdCrc.size = 4;
+    comTxComCmdCrc.data = (uint8_t *)&comCmdCrc;
+    comTxComCmdCrc.size = 4;
 
-	comTxComCmdTmo.data = (uint8_t*) &comCmdTmo;
-	comTxComCmdTmo.size = 4;
+    comTxComCmdTmo.data = (uint8_t *)&comCmdTmo;
+    comTxComCmdTmo.size = 4;
 
-	// initialize shared variables
-	mcuRxBatVolt.data = (uint8_t*) &bat_v;
-	mcuRxBatVolt.size = 4;
+    // initialize shared variables
+    mcuRxBatVolt.data = (uint8_t*) &bat_v;
+    mcuRxBatVolt.size = 4;
 
-	mcuRx4vCurr.data = (uint8_t*) &i4;
-	mcuRx4vCurr.size = 4;
+    mcuRx4vCurr.data = (uint8_t*) &i4;
+    mcuRx4vCurr.size = 4;
 
-	mcuRx3v3Curr.data = (uint8_t*) &i3_3;
-	mcuRx3v3Curr.size = 4;
+    mcuRx3v3Curr.data = (uint8_t*) &i3_3;
+    mcuRx3v3Curr.size = 4;
 
-	mcuRxTemp.data = (uint8_t*) &temp;
-	mcuRxTemp.size = 4;
+    mcuRx3v3.data = (uint8_t*) &vdda;
+    mcuRx3v3.size = 4;
 
-	testVarRx.data = (uint8_t*) &testVariable;
-	testVarRx.size = 4;
+    mcuRxTemp.data = (uint8_t*) &temp;
+    mcuRxTemp.size = 4;
 
-	// initialize remote commands
-	mcuTxBurn.CMD_CB = HQ_BURN_WORLD;
-	mcuTxBurn.data = &burnTime;
-	mcuTxBurn.size = 1;
+    mcuRxTempIn.data = (uint8_t*) &temp_in;
+    mcuRxTempIn.size = 4;
 
-	comTrxBuzz.CMD_CB = HQ_BUZZZ;
-	comTrxBuzz.data = &buzzTime;
-	comTrxBuzz.size = 1;
+    mcuRxTempOut.data = (uint8_t*) &temp_out;
+    mcuRxTempOut.size = 4;
 
-	// this is what can be requested
-	comTrxHandle.countDataRx = 12;
-	comTrxHandle.dataRx = rxDataArray;
-	// some timings
-	comTrxHandle.rxRetries = 3;
-	comTrxHandle.rxTimeout = 20000;
-	// this is what we can request
-	comTrxHandle.countDataTx = 10;
-	comTrxHandle.dataTx = txDataArray;
-	// this is commands that can be executed here
-	comTrxHandle.countCmdRx = 2;
-	comTrxHandle.cmdRx = txCmdArray;
-	// other config
-	comTrxHandle.uart = handle->uart;
-	BOARDTRX_Status ret;
-	ret = BOARDTRX_Init(&comTrxHandle);
+    mcuRxTempBme.data = (uint8_t*) &temp_bme;
+    mcuRxTempBme.size = 4;
 
-	if (ret.isNewError) {
-		return HQ_ERROR;
-	}
+    mcuRxPressure.data = (uint8_t*) &bme_pressure;
+    mcuRxPressure.size = 4;
 
-	return HQ_OK;
+    mcuRxHumid.data = (uint8_t*) &bme_humid;
+    mcuRxHumid.size = 4;
+
+    mcuRxAirQ.data = (uint8_t*) &bme_air_q;
+    mcuRxAirQ.size = 4;
+
+    mcuRxGasOhm.data = (uint8_t*) &bme_air_ohm;
+    mcuRxGasOhm.size = 4;
+
+    mcuRxSenState.data = (uint8_t*) &senStatus;
+    mcuRxSenState.size = 4;
+
+    // initialize remote commands
+    mcuTxBurn.CMD_CB = HQ_BURN_WORLD;
+    mcuTxBurn.data = &burnTime;
+    mcuTxBurn.size = 1;
+
+    comTrxBuzz.CMD_CB = HQ_BUZZZ;
+    comTrxBuzz.data = &buzzTime;
+    comTrxBuzz.size = 1;
+
+    // this is what can be requested
+    comTrxHandle.countDataRx = HQ_CMD_RX_END;
+    comTrxHandle.dataRx = rxDataArray;
+    // some timings
+    comTrxHandle.txRetries = 3;
+    comTrxHandle.txTimeout = 20000;
+    // this is what we can request
+    comTrxHandle.countDataTx = HQ_CMD_TX_END;
+    comTrxHandle.dataTx = txDataArray;
+    // this is commands that can be executed here
+    comTrxHandle.countCmdRx = 2;
+    comTrxHandle.cmdRx = txCmdArray;
+    // other config
+    comTrxHandle.uart = handle->uart;
+    BOARDTRX_Status ret;
+    ret = BOARDTRX_Init(&comTrxHandle);
+
+    if (ret.isNewError) {
+        return HQ_ERROR;
+    }
+
+    return HQ_OK;
 }
 
 HQ_StatusTypeDef HQ_Loop() {
-	// TODO process ret
-	BOARDTRX_Status ret;
-	ret = BOARDTRX_Loop();
+    // TODO process ret
+    BOARDTRX_Status ret;
+    ret = BOARDTRX_Loop();
 
-	return HQ_IDLE;
+    return HQ_IDLE;
 }
 
 /*
@@ -206,10 +236,10 @@ HQ_StatusTypeDef HQ_Loop() {
  *          global variables
  */
 void HQ_BURN_WORLD() {
-	for (uint8_t i = 0; i < burnTime; i++) {
-		printf("BURNING THINGS \n\r");
-		HAL_Delay(1000);
-	}
+    for (uint8_t i = 0; i < burnTime; i++) {
+        printf("BURNING THINGS \n\r");
+        HAL_Delay(1000);
+    }
 }
 
 /*
@@ -219,9 +249,9 @@ void HQ_BURN_WORLD() {
  */
 
 void HQ_BUZZZ() {
-	for (uint8_t i = 0; i < buzzTime; i++) {
-		printf("Do you hear bees? \n\r");
-		HAL_Delay(1000);
-	}
+    for (uint8_t i = 0; i < buzzTime; i++) {
+        printf("Do you hear bees? \n\r");
+        HAL_Delay(1000);
+    }
 }
 
